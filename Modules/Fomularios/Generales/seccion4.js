837dce213285dc8594e0ee7d0783246e7f2fac2a
Rafaelrc <rafaelrc2001@gmail.com>
2025-09-01 22:23:03 -0600

    falta que se agregue empresa y solicitante en apertura equipo linea

--- Modules/Fomularios/Generales/seccion4.js
+++ Modules/Fomularios/Generales/seccion4.js
@@ -382,7 +382,7 @@ const tag = document.getElementById('tag')?.value || null;
 const fecha = document.getElementById('permit-date')?.value || '';
 const hora = document.getElementById('start-time')?.value || '';
 const hora_inicio = `${fecha} ${hora}`;
-const tiene_equipo_intervenir = textoABoolean(document.querySelector('input[name="has-equipment"]:checked')?.value || null);
+const tiene_equipo_intervenir = document.querySelector('input[name="has-equipment"]:checked')?.value || null;
 const descripcion_equipo = document.getElementById('equipment-description')?.value || null;
 const fluido = document.getElementById('fluid')?.value || null;
 const presion = document.getElementById('pressure')?.value || null;
@@ -390,37 +390,37 @@ const temperatura = document.getElementById('temperature')?.value || null;
 const antecedentes = document.getElementById('background')?.value || null;
 
 // Medidas para administrar los riesgos
-const requiere_herramientas_especiales = textoABoolean(document.querySelector('input[name="special-tools"]:checked')?.value || null);
+const requiere_herramientas_especiales = document.querySelector('input[name="special-tools"]:checked')?.value || null;
 const tipo_herramientas_especiales = document.getElementById('special-tools-type')?.value || null;
-const herramientas_adecuadas = textoABoolean(document.querySelector('input[name="adequate-tools"]:checked')?.value || null);
-const requiere_verificacion_previa = textoABoolean(document.querySelector('input[name="pre-verification"]:checked')?.value || null);
-const requiere_conocer_riesgos = textoABoolean(document.querySelector('input[name="risk-knowledge"]:checked')?.value || null);
+const herramientas_adecuadas = document.querySelector('input[name="adequate-tools"]:checked')?.value || null;
+const requiere_verificacion_previa = document.querySelector('input[name="pre-verification"]:checked')?.value || null;
+const requiere_conocer_riesgos = document.querySelector('input[name="risk-knowledge"]:checked')?.value || null;
 const observaciones_medidas = document.getElementById('final-observations')?.value || null;
 
 // Requisitos para efectuar el trabajo
-const fuera_operacion = textoABoolean(document.querySelector('input[name="out-of-operation"]:checked')?.value || null);
-const despresurizado_purgado = textoABoolean(document.querySelector('input[name="depressurized"]:checked')?.value || null);
-const necesita_aislamiento = textoABoolean(document.querySelector('input[name="isolated"]:checked')?.value || null);
-const con_valvulas = textoABoolean(document.querySelector('input[name="with-valves"]:checked')?.value || null);
-const con_juntas_ciegas = textoABoolean(document.querySelector('input[name="with-blinds"]:checked')?.value || null);
-const producto_entrampado = textoABoolean(document.querySelector('input[name="trapped-product"]:checked')?.value || null);
-const requiere_lavado = textoABoolean(document.querySelector('input[name="requires-washing"]:checked')?.value || null);
-const requiere_neutralizado = textoABoolean(document.querySelector('input[name="requires-neutralization"]:checked')?.value || null);
-const requiere_vaporizado = textoABoolean(document.querySelector('input[name="requires-steaming"]:checked')?.value || null);
-const suspender_trabajos_adyacentes = textoABoolean(document.querySelector('input[name="adjacent-work-suspended"]:checked')?.value || null);
-const acordonar_area = textoABoolean(document.querySelector('input[name="area-cordoned"]:checked')?.value || null);
-const prueba_gas_toxico_inflamable = textoABoolean(document.querySelector('input[name="gas-test-required"]:checked')?.value || null);
-const equipo_electrico_desenergizado = textoABoolean(document.querySelector('input[name="de-energized"]:checked')?.value || null);
-const tapar_purgas_drenajes = textoABoolean(document.querySelector('input[name="drains-covered"]:checked')?.value || null);
+const fuera_operacion = document.querySelector('input[name="out-of-operation"]:checked')?.value || null;
+const despresurizado_purgado = document.querySelector('input[name="depressurized"]:checked')?.value || null;
+const necesita_aislamiento = document.querySelector('input[name="isolated"]:checked')?.value || null;
+const con_valvulas = document.querySelector('input[name="with-valves"]:checked')?.value || null;
+const con_juntas_ciegas = document.querySelector('input[name="with-blinds"]:checked')?.value || null;
+const producto_entrampado = document.querySelector('input[name="trapped-product"]:checked')?.value || null;
+const requiere_lavado = document.querySelector('input[name="requires-washing"]:checked')?.value || null;
+const requiere_neutralizado = document.querySelector('input[name="requires-neutralization"]:checked')?.value || null;
+const requiere_vaporizado = document.querySelector('input[name="requires-steaming"]:checked')?.value || null;
+const suspender_trabajos_adyacentes = document.querySelector('input[name="adjacent-work-suspended"]:checked')?.value || null;
+const acordonar_area = document.querySelector('input[name="area-cordoned"]:checked')?.value || null;
+const prueba_gas_toxico_inflamable = document.querySelector('input[name="gas-test-required"]:checked')?.value || null;
+const equipo_electrico_desenergizado = document.querySelector('input[name="de-energized"]:checked')?.value || null;
+const tapar_purgas_drenajes = document.querySelector('input[name="drains-covered"]:checked')?.value || null;
 
 // Requisitos para administrar los riesgos
-const proteccion_especial_recomendada = textoABoolean(document.querySelector('input[name="special-protection"]:checked')?.value || null);
-const proteccion_piel_cuerpo = textoABoolean(document.querySelector('input[name="skin-protection"]:checked')?.value || null);
-const proteccion_respiratoria = textoABoolean(document.querySelector('input[name="respiratory-protection"]:checked')?.value || null);
-const proteccion_ocular = textoABoolean(document.querySelector('input[name="eye-protection"]:checked')?.value || null);
-const proteccion_contraincendio = textoABoolean(document.querySelector('input[name="fire-protection"]:checked')?.value || null);
+const proteccion_especial_recomendada = document.querySelector('input[name="special-protection"]:checked')?.value || null;
+const proteccion_piel_cuerpo = document.querySelector('input[name="skin-protection"]:checked')?.value || null;
+const proteccion_respiratoria = document.querySelector('input[name="respiratory-protection"]:checked')?.value || null;
+const proteccion_ocular = document.querySelector('input[name="eye-protection"]:checked')?.value || null;
+const proteccion_contraincendio = document.querySelector('input[name="fire-protection"]:checked')?.value || null;
 const tipo_proteccion_contraincendio = document.getElementById('fire-protection-type')?.value || null;
-const instalacion_barreras = textoABoolean(document.querySelector('input[name="barriers-required"]:checked')?.value || null);
+const instalacion_barreras = document.querySelector('input[name="barriers-required"]:checked')?.value || null;
 const observaciones_riesgos = document.getElementById('observations')?.value || null;
 
 // Registro de pruebas requeridas
@@ -740,18 +740,29 @@ router.post('/api/pt-no-peligroso', async (req, res) => {
 router.post('/api/pt-apertura', async (req, res) => {
     const { id_permiso, tipo_mantenimiento, otro_tipo_mantenimiento, ot_numero, tag, hora_inicio, tiene_equipo_intervenir, descripcion_equipo, fluido, presion, temperatura, antecedentes, requiere_herramientas_especiales, tipo_herramientas_especiales, herramientas_adecuadas, requiere_verificacion_previa, requiere_conocer_riesgos, observaciones_medidas, fuera_operacion, despresurizado_purgado, necesita_aislamiento, con_valvulas, con_juntas_ciegas, producto_entrampado, requiere_lavado, requiere_neutralizado, requiere_vaporizado, suspender_trabajos_adyacentes, acordonar_area, prueba_gas_toxico_inflamable, equipo_electrico_desenergizado, tapar_purgas_drenajes, proteccion_especial_recomendada, proteccion_piel_cuerpo, proteccion_respiratoria, proteccion_ocular, proteccion_contraincendio, tipo_proteccion_contraincendio, instalacion_barreras, observaciones_riesgos, co2_nivel, nh3_nivel, oxigeno_nivel, lel_nivel } = req.body;
 
-    // Validar campos obligatorios
-    if (!id_permiso || !tipo_mantenimiento || !hora_inicio) {
-        return res.status(400).json({
-            success: false,
-            error: 'Faltan campos obligatorios para pt_apertura'
-        });
-    }
+ const datosApertura = {
+    id_permiso: idPermisoGenerado, // Debe ser un número válido
+    tipo_mantenimiento: tipo_mantenimiento, // Debe ser un string válido
+    hora_inicio: hora_inicio, // Debe ser un string con fecha y hora
+    // ...todos los demás campos...
+};
 
     const query = `
-        INSERT INTO pt_apertura (id_permiso, tipo_mantenimiento, otro_tipo_mantenimiento, ot_numero, tag, hora_inicio, tiene_equipo_intervenir, descripcion_equipo, fluido, presion, temperatura, antecedentes, requiere_herramientas_especiales, tipo_herramientas_especiales, herramientas_adecuadas, requiere_verificacion_previa, requiere_conocer_riesgos, observaciones_medidas, fuera_operacion, despresurizado_purgado, necesita_aislamiento, con_valvulas, con_juntas_ciegas, producto_entrampado, requiere_lavado, requiere_neutralizado, requiere_vaporizado, suspender_trabajos_adyacentes, acordonar_area, prueba_gas_toxico_inflamable, equipo_electrico_desenergizado, tapar_purgas_drenajes, proteccion_especial_recomendada, proteccion_piel_cuerpo, proteccion_respiratoria, proteccion_ocular, proteccion_contraincendio, tipo_proteccion_contraincendio, instalacion_barreras, observaciones_riesgos, co2_nivel, nh3_nivel, oxigeno_nivel, lel_nivel)
-        VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20, $21, $22, $23, $24, $25, $26, $27, $28, $29, $30, $31, $32, $33, $34, $35, $36, $37, $38, $39, $40, $41, $42, $43, $44, $45, $46, $47, $48, $49, $50, $51, $52, $53, $54, $55, $56, $57, $58, $59, $60, $61, $62, $63, $64, $65, $66, $67, $68, $69, $70, $71, $72, $73, $74, $75, $76, $77, $78, $79, $80, $81, $82, $83, $84, $85, $86, $87, $88, $89, $90, $91, $92, $93, $94, $95, $96, $97, $98, $99, $100, $101, $102, $103, $104, $105, $106, $107, $108, $109, $110, $111, $112, $113, $114, $115, $116, $117, $118, $119, $120, $121, $122, $123, $124, $125, $126, $127, $128, $129, $130, $131, $132, $133, $134, $135, $136, $137, $138, $139, $140, $141, $142, $143, $144, $145, $146, $147, $148, $149, $150, $151, $152, $153, $154, $155, $156, $157, $158, $159, $160, $161, $162, $163, $164, $165, $166, $167, $168, $169, $170, $171, $172, $173, $174, $175, $176, $177, $178, $179, $180, $181, $182, $183, $184, $185, $186, $187, $188, $189, $190, $191, $192, $193, $194, $195, $196, $197, $198, $199, $200, $201, $202, $203, $204, $205, $206, $207, $208, $209, $210, $211, $212, $213, $214, $215, $216, $217, $218, $219, $220, $221, $222, $223, $224, $225, $226, $227, $228, $229, $230, $231, $232, $233, $234, $235, $236, $237, $238, $239, $240, $241, $242, $243, $244, $245, $246, $247, $248, $249, $250, $251, $252, $253, $254, $255, $256, $257, $258, $259, $260, $261, $262, $263, $264, $265, $266, $267, $268, $269, $270, $271, $272, $273, $274, $275, $276, $277, $278, $279, $280, $281, $282, $283, $284, $285, $286, $287, $288, $289, $290, $291, $292, $293, $294, $295, $296, $297, $298, $299, $300, $301, $302, $303, $304, $305, $306, $307, $308, $309, $310, $311, $312, $313, $314, $315, $316, $317, $318, $319, $320, $321, $322, $323, $324, $325, $326, $327, $328, $329, $330, $331, $332, $333, $334, $335, $336, $337, $338, $339, $340, $341, $342, $343, $344, $345, $346, $347, $348, $349, $350, $351, $352, $353, $354, $355, $356, $357, $358, $359, $360, $361, $362, $363, $364, $365, $366, $367, $368, $369, $370, $371, $372, $373, $374, $375, $376, $377, $378, $379, $380, $381, $382, $383, $384, $385, $386, $387, $388, $389, $390, $391, $392, $393, $394, $395, $396, $397, $398, $399, $400, $401, $402, $403, $404, $405, $406, $407, $408, $409, $410, $411, $412, $413, $414, $415, $416, $417, $418, $419, $420, $421, $422, $423, $424, $425, $426, $427, $428, $429, $430, $431, $432, $433, $434, $435, $436, $437, $438, $439, $440, $441, $442, $443, $444, $445, $446, $447, $448, $449, $450, $451, $452, $453, $454, $455, $456, $457, $458, $459, $460, $461, $462, $463, $464, $465, $466, $467, $468, $469, $470, $471, $472, $473, $474, $475, $476, $477, $478, $479, $480, $481, $482, $483, $484, $485, $486, $487, $488, $489, $490, $491, $492, $493, $494, $495, $496, $497, $498, $499, $500, $501, $502, $503, $504, $505, $506, $507, $508, $509, $510, $511, $512, $513, $514, $515, $516, $517, $518, $519, $520, $521, $522, $523, $524, $525, $526, $527, $528, $529, $530, $531, $532, $533, $534, $535, $536, $537, $538, $539, $540, $541, $542, $543, $544, $545, $546, $547, $548, $549, $550, $551, $552, $553, $554, $555, $556, $557, $558, $559, $560, $561, $562, $563, $564, $565, $566, $567, $568, $569, $570, $571, $572, $573, $574, $575, $576, $577, $578, $579, $580, $581, $582, $583, $584, $585, $586, $587, $588, $589, $590, $591, $592, $593, $594, $595, $596, $597, $598, $599, $600, $601, $602, $603, $604, $605, $606, $607, $608, $609, $610, $611, $612, $613, $614, $615, $616, $617, $618, $619, $620, $621, $622, $623, $624, $625, $626, $627, $628, $629
-    `;
+    INSERT INTO pt_apertura (
+        id_permiso, tipo_mantenimiento, otro_tipo_mantenimiento, ot_numero, tag, hora_inicio, tiene_equipo_intervenir, descripcion_equipo,
+        fluido, presion, temperatura, antecedentes, requiere_herramientas_especiales, tipo_herramientas_especiales, herramientas_adecuadas,
+        requiere_verificacion_previa, requiere_conocer_riesgos, observaciones_medidas, fuera_operacion, despresurizado_purgado,
+        necesita_aislamiento, con_valvulas, con_juntas_ciegas, producto_entrampado, requiere_lavado, requiere_neutralizado,
+        requiere_vaporizado, suspender_trabajos_adyacentes, acordonar_area, prueba_gas_toxico_inflamable, equipo_electrico_desenergizado,
+        tapar_purgas_drenajes, proteccion_especial_recomendada, proteccion_piel_cuerpo, proteccion_respiratoria, proteccion_ocular,
+        proteccion_contraincendio, tipo_proteccion_contraincendio, instalacion_barreras, observaciones_riesgos, co2_nivel, nh3_nivel,
+        oxigeno_nivel, lel_nivel
+    )
+    VALUES (
+        $1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20, $21, $22, $23, $24, $25, $26, $27, $28, $29, $30, $31, $32, $33, $34, $35, $36, $37, $38, $39, $40, $41, $42, $43, $44
+    )
+    RETURNING *;
+`;
     const values = [id_permiso, tipo_mantenimiento, otro_tipo_mantenimiento, ot_numero, tag, hora_inicio, tiene_equipo_intervenir, descripcion_equipo, fluido, presion, temperatura, antecedentes, requiere_herramientas_especiales, tipo_herramientas_especiales, herramientas_adecuadas, requiere_verificacion_previa, requiere_conocer_riesgos, observaciones_medidas, fuera_operacion, despresurizado_purgado, necesita_aislamiento, con_valvulas, con_juntas_ciegas, producto_entrampado, requiere_lavado, requiere_neutralizado, requiere_vaporizado, suspender_trabajos_adyacentes, acordonar_area, prueba_gas_toxico_inflamable, equipo_electrico_desenergizado, tapar_purgas_drenajes, proteccion_especial_recomendada, proteccion_piel_cuerpo, proteccion_respiratoria, proteccion_ocular, proteccion_contraincendio, tipo_proteccion_contraincendio, instalacion_barreras, observaciones_riesgos, co2_nivel, nh3_nivel, oxigeno_nivel, lel_nivel];
 
     try {

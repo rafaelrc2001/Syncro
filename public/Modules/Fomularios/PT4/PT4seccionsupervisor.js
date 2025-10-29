69bf4fbf3605ea9fa73df2f80e8b4077b453d8ad
Diego Ramirez <rafaelrc2001@gmail.com>
2025-10-29 10:54:22 -0600

    Fix ruta exportar

--- public/Modules/Fomularios/PT4/PT4seccionsupervisor.js
+++ public/Modules/Fomularios/PT4/PT4seccionsupervisor.js
@@ -423,7 +423,7 @@ document.addEventListener("DOMContentLoaded", function () {
           const modal = document.getElementById("modalComentario");
           if (modal) modal.style.display = "none";
           alert("Permiso no autorizado correctamente");
-          window.location.href = "/Modules/SupSeguridad/Supseguridad.html";
+          window.location.href = "/Modules/SupSeguridad/SupSeguridad.html";
         };
       }
       // Lógica para cerrar/cancelar el modal

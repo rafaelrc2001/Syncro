473222ac819817aff00383756e6093a818da8c64
Diego Ramirez <rafaelrc2001@gmail.com>
2026-01-29 15:49:44 -0600

    a

--- public/Modules/Fomularios/Generales/notificacionPermisoActivo.js
+++ public/Modules/Fomularios/Generales/notificacionPermisoActivo.js
@@ -112,7 +112,7 @@ window.notificacionPermisoActivoHandler = async function () {
 		const response = await fetch(
 			//"https://7mhxkntt-5678.usw3.devtunnels.ms/webhook-test/permisoactivo", 
             //"https://7mhxkntt-5678.usw3.devtunnels.ms/webhook/permisoactivo", 
-			"https://n8n.proagroindustria.com/webhook-test/permisoactivo",
+			"https://n8n.proagroindustria.com/webhook/permisoactivo",
 			{
 				method: "POST",
 				headers: {

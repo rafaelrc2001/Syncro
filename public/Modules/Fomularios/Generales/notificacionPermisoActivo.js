89e184e1d56bfccc517ac9bc47af4596ae0ca7e9
Diego Ramirez <rafaelrc2001@gmail.com>
2026-01-29 17:21:32 -0600

    a

--- public/Modules/Fomularios/Generales/notificacionPermisoActivo.js
+++ public/Modules/Fomularios/Generales/notificacionPermisoActivo.js
@@ -113,7 +113,8 @@ window.notificacionPermisoActivoHandler = async function () {
 			//"https://7mhxkntt-5678.usw3.devtunnels.ms/webhook-test/permisoactivo", 
             //"https://7mhxkntt-5678.usw3.devtunnels.ms/webhook/permisoactivo", 
 			  //"https://n8n.proagroindustria.com/webhook/permisoactivo",
-			  "https://n8n.proagroindustria.com/webhook/permisos-activos",
+			  //"https://n8n.proagroindustria.com/webhook/permisos-activos",
+			  "https://n8n.proagroindustria.com/webhook/permiso-activo",
         
 			{
 				method: "POST",
